a919f591d0d79acdc0a13ea76e0e26a75d3292b6
Mika Laihanen <mika.laihanen@gmail.com>
2024-03-24 20:35:00 +0200

    Add initial render mechanism for bus times

--- src/BusTimes.js
+++ src/BusTimes.js
@@ -16,46 +16,26 @@ const getDifference = (time) => {
   const differenceInMilliseconds = arrivalTime - currentTime
   const differenceInSeconds = Math.floor(differenceInMilliseconds / 1000);
   let seconds = differenceInSeconds % 60;
-  if (seconds < 10 && seconds > -1) {
-    seconds = "0" + Math.abs(seconds);
-  }
+  
   const differenceInMinutes = Math.floor(differenceInSeconds / 60)
   const minutes = differenceInMinutes % 60
-
   return [minutes, seconds]
-
 }
 
 export function BusTimes() {
   const { isError, isLoading, data, error } = useQuery({queryKey: ["stopTimes"], queryFn: () => fetchPosts("0815")})
 
-  if (isLoading) return "loading"
+  if (isLoading) return "Loading"
 
-  if (isError) return `Errori ${error.message}`
+  if (isError) return `Error: ${error.message}`
   
-// console.log(data.body["0815"][0].call)
-
-  const [minutes, seconds] = getDifference(data.body["0815"][0].call.expectedArrivalTime)
-
   return (
     <div className="schedule-container">
-      {/* {data.body && data.body["0815"].map((test, index) => { */}
-      {data.body && 
-        <TimeDisplay minutes={minutes} seconds={seconds} />
-
-      
+      {data.body && data.body["0815"].map((item) => {
+        const [minutes, seconds] = getDifference(item.call.expectedArrivalTime);
+        return <TimeDisplay minutes={minutes} seconds={seconds} key={item.vehicleRef}/>
+      })
       }
-
-      
-      
-        // (
-        //   <>
-        //   {/* <div>Arriving in</div>
-        //   <div>{getDifference(test.call.expectedArrivalTime)}</div> */}
-
-          
-        //   </>
-        // )
     </div>
   )
 }
--- src/TimeDisplay.js
+++ src/TimeDisplay.js
@@ -1,33 +1,51 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
+
+const calculateNewSec = (second, minute) => {
+  if (second - 1 < 0 && minute > 0) {
+    return 59;
+  }
+  if (second -1 < 0 && minute === 0) {
+    return 0;
+  }
+  return second - 1;
+}
+
+const calculateNewMin = (minute) => {
+  if (minute - 1 < 0) return 0
+  return minute - 1;
+}
+
+const formatSecond = (second) => {
+  if (second < 10) return `0${second}`
+  return second;
+}
 
 export function TimeDisplay (props) {
   const { minutes, seconds } = props;
-  // console.log(typeof minutes, typeof seconds)
+  const interval = useRef(null)
 
   const [myMinute, setMyMinute] = useState(minutes);
   const [mySecond, setMySecond] = useState(seconds);
 
   useEffect(() => {
-    const setSecond = setInterval(() => {
-      // let newSecond = mySecond - 1;
-      // let newMinute;
-      // console.log(newSecond)
-  
-      // if (newSecond < 0) {
-      //   newSecond = 59;
-      //   newMinute = myMinute - 1;
-      //   setMyMinute(newMinute)
-      // }
-      setMySecond((prevSecond) => prevSecond - 1);
+    interval.current = setInterval(() => {
+      if (myMinute === 0 && mySecond === 0) return;
+      setMySecond((prevSecond) => {
+        const newSec = calculateNewSec(prevSecond, myMinute);
+        if (newSec === 59) {
+          setMyMinute(prevMin => calculateNewMin(prevMin))
+        }
+        return newSec
+      });
     }, 1000)
-    // const setSecond = setInterval(() => {
-    //   console.log("TEST")
-    // }, 100)
 
-    return () => clearInterval(setSecond)
+    if (myMinute === 0 && mySecond === 0) clearInterval(interval.current)
+
+    return () => clearInterval(interval.current)
 
   }, [])
 
+  if (myMinute === 0 && mySecond === 0) { clearInterval(interval.current); return false};
   
-  return (<p>{`${myMinute}:${mySecond}`}</p>)
+  return (<p>{`${myMinute}:${formatSecond(mySecond)}`}</p>)
 }
